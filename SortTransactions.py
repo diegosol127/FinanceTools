59fd035d7091a44068c8727b49ffdd8b8aecbe30
diegosol127 <diegosol127@gmail.com>
2024-10-26 23:59:44 -0600

    Create parser class for cleaner code

--- SortTransactions.py
+++ SortTransactions.py
@@ -1,10 +1,13 @@
-# Import libraries
+# Import Python libraries
 import argparse
 import json
 import os
 import sys
 import numpy as np
 import pandas as pd
+
+# Import custom libraries
+from Utilities.parser import Parser
 
 def get_classification_info(directory):
     # Get a list of all JSON files in a given directory
@@ -33,65 +36,6 @@ def group_transactions(description, classification_dict):
                 return category
     return '***Unlabelled***'
 
-def parse_AmericanExpress(file_path):
-    # Parse CSV files in the format given by American Express
-    df = pd.read_csv(file_path, header=None)
-    df.drop(0, axis=0, inplace=True)
-    df = df[df.columns[[0,2,1]]]
-    df.rename(columns={0: 'DATE', 2: 'AMOUNT', 1: 'DESCRIPTION'}, inplace=True)
-    df['AMOUNT'] = -df['AMOUNT'].astype(float)
-    df['INSTITUTION'] = 'AmericanExpress'
-    return df
-
-def parse_Optum(file_path):
-    # Parse CSV files in the format given by Optum
-    df = pd.read_csv(file_path, header=None)
-    df.drop(0, axis=0, inplace=True)
-    df.drop(3, axis=1, inplace=True)
-    df = df[df.columns[[0,2,1]]]
-    df.rename(columns={0: 'DATE', 2: 'AMOUNT', 1: 'DESCRIPTION'}, inplace=True)
-    df['AMOUNT'] = df['AMOUNT'].str.strip('$').astype(float)
-    df['INSTITUTION'] = 'Optum'
-    return df
-
-def parse_SoFi(file_path):
-    # Parse CSV files in the format given by SoFi
-    df = pd.read_csv(file_path, header=None)
-    df.drop(0, axis=0, inplace=True)
-    df.drop(2, axis=1, inplace=True)
-    df.drop(4, axis=1, inplace=True)
-    df.drop(5, axis=1, inplace=True)
-    df = df[df.columns[[0,2,1]]]
-    df.rename(columns={0: 'DATE', 3: 'AMOUNT', 1: 'DESCRIPTION'}, inplace=True)
-    df['AMOUNT'] = df['AMOUNT'].astype(float)
-    df['INSTITUTION'] = 'SoFi'
-    return df
-
-def parse_WellsFargo(file_path):
-    # Parse CSV files in the format given by Wells Fargo
-    df = pd.read_csv(file_path, header=None)
-    df.drop([2,3],axis=1, inplace=True)
-    df.rename(columns={0: 'DATE', 1: 'AMOUNT', 4: 'DESCRIPTION'}, inplace=True)
-    df['DESCRIPTION'] = df['DESCRIPTION'].str.replace(',','')
-    df['DATE'] = pd.to_datetime(df['DATE'], format='%m/%d/%Y').dt.strftime('%Y-%m-%d')
-    df['INSTITUTION'] = 'Wells Fargo'
-    return df
-    
-def parse_file(file_path):
-    # Return a data parser based on the name of the institution
-    parsers = {
-        'AmericanExpress': parse_AmericanExpress,
-        'Optum': parse_Optum,
-        'WellsFargo': parse_WellsFargo,
-        'SoFi': parse_SoFi
-    }
-
-    institution = os.path.basename(os.path.dirname(file_path))
-    if institution in parsers:
-        return parsers[institution](file_path)
-    else:
-        raise ValueError(f"No parser available for institution: {institution}")
-    
 def extract_data(directory,date):
     # Extract the csv data from the selected directory
     df_combined = pd.DataFrame()
@@ -101,7 +45,8 @@ def extract_data(directory,date):
             if date in file:
                 file_path = os.path.join(root, file)
                 try:
-                    df = parse_file(file_path)
+                    parser = Parser()
+                    df = parser.parse_file(file_path)
                     df_combined = pd.concat([df_combined,df],axis=0, ignore_index=True)
                     print(f"Processed {file_path}")
                 except ValueError as e:
---
+++ Utilities/parser.py
@@ -0,0 +1,67 @@
+# Import Python libraries
+import os
+import pandas as pd
+
+# Transaction parser class
+class Parser:
+    def __init__(self) -> None:
+        pass
+    
+    def parse_file(self, file_path):
+        # Return a data parser based on the name of the institution
+        parsers = {
+            'AmericanExpress': self.parse_AmericanExpress,
+            'Optum': self.parse_Optum,
+            'WellsFargo': self.parse_WellsFargo,
+            'SoFi': self.parse_SoFi
+        }
+
+        institution = os.path.basename(os.path.dirname(file_path))
+        if institution in parsers:
+            return parsers[institution](file_path)
+        else:
+            raise ValueError(f"No parser available for institution: {institution}")
+    
+    def parse_AmericanExpress(self, file_path):
+        # Parse CSV files in the format given by American Express
+        df = pd.read_csv(file_path, header=None)
+        df.drop(0, axis=0, inplace=True)
+        df = df[df.columns[[0,2,1]]]
+        df.rename(columns={0: 'DATE', 2: 'AMOUNT', 1: 'DESCRIPTION'}, inplace=True)
+        df['AMOUNT'] = -df['AMOUNT'].astype(float)
+        df['INSTITUTION'] = 'AmericanExpress'
+        return df
+
+    def parse_Optum(self, file_path):
+        # Parse CSV files in the format given by Optum
+        df = pd.read_csv(file_path, header=None)
+        df.drop(0, axis=0, inplace=True)
+        df.drop(3, axis=1, inplace=True)
+        df = df[df.columns[[0,2,1]]]
+        df.rename(columns={0: 'DATE', 2: 'AMOUNT', 1: 'DESCRIPTION'}, inplace=True)
+        df['AMOUNT'] = df['AMOUNT'].str.strip('$').astype(float)
+        df['INSTITUTION'] = 'Optum'
+        return df
+
+    def parse_SoFi(self, file_path):
+        # Parse CSV files in the format given by SoFi
+        df = pd.read_csv(file_path, header=None)
+        df.drop(0, axis=0, inplace=True)
+        df.drop(2, axis=1, inplace=True)
+        df.drop(4, axis=1, inplace=True)
+        df.drop(5, axis=1, inplace=True)
+        df = df[df.columns[[0,2,1]]]
+        df.rename(columns={0: 'DATE', 3: 'AMOUNT', 1: 'DESCRIPTION'}, inplace=True)
+        df['AMOUNT'] = df['AMOUNT'].astype(float)
+        df['INSTITUTION'] = 'SoFi'
+        return df
+
+    def parse_WellsFargo(self, file_path):
+        # Parse CSV files in the format given by Wells Fargo
+        df = pd.read_csv(file_path, header=None)
+        df.drop([2,3],axis=1, inplace=True)
+        df.rename(columns={0: 'DATE', 1: 'AMOUNT', 4: 'DESCRIPTION'}, inplace=True)
+        df['DESCRIPTION'] = df['DESCRIPTION'].str.replace(',','')
+        df['DATE'] = pd.to_datetime(df['DATE'], format='%m/%d/%Y').dt.strftime('%Y-%m-%d')
+        df['INSTITUTION'] = 'Wells Fargo'
+        return df
